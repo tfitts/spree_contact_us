daf1ba1327cebebd7c23c0398201af93dcc8b6ac
Tyler Fitts <tfitts@gmail.com>
2013-09-03 11:07:44 -0600

    Added I18n prefix to translation.

--- app/controllers/spree/contact_us/contacts_controller.rb
+++ app/controllers/spree/contact_us/contacts_controller.rb
@@ -8,7 +8,7 @@ class Spree::ContactUs::ContactsController < Spree::StoreController
       if Spree::ContactUs::Config.contact_tracking_message.present?
         flash[:contact_tracking] = Spree::ContactUs::Config.contact_tracking_message
       end
-      redirect_to(spree.root_path, :notice => t('spree.contact_us.notices.success'))
+      redirect_to(spree.root_path, :notice => I18n.t('spree.contact_us.notices.success'))
     else
       render :new
     end
@@ -21,7 +21,7 @@ class Spree::ContactUs::ContactsController < Spree::StoreController
   private
 
   def accurate_title
-    t('contact_us')
+    I18n.t('contact_us')
   end
 
 end
--- app/mailers/spree/contact_us/contact_mailer.rb
+++ app/mailers/spree/contact_us/contact_mailer.rb
@@ -4,7 +4,7 @@ class Spree::ContactUs::ContactMailer < ActionMailer::Base
 
     mail :from     => (SpreeContactUs.mailer_from || @contact.email),
          :reply_to => @contact.email,
-         :subject  => (SpreeContactUs.require_subject ? @contact.subject : t('subject', :email => @contact.email)),
+         :subject  => (SpreeContactUs.require_subject ? @contact.subject : I18n.t('subject', :email => @contact.email)),
          :to       => SpreeContactUs.mailer_to
   end
 end
